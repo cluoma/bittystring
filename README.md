1e42f2907ea1cfc26e901f97c5d69aaac590ece5
Colin Luoma <themjolnir999@gmail.com>
2021-01-01 23:10:04 +0100

    Update readme and add more tests

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # bittystring
-A C string library with short-string optimization
+A C string library with short-string optimization. Supports short string up to 23 characters (22 + null terminator).
+
+# Examples
+
+### Heap Allocate
+```c
+char *s = "Cool text";
+
+bstr *bs = bstr_new();
+bstr_append_from_cstring(bs, s, strlen(s));
+bstr_free(bs);
+```
+
+### Stack Allocate
+```c
+char *s = "Cool text";
+
+bstr bs;
+bstr_init(&bs);
+bstr_append_from_cstring(&bs, s, strlen(s));
+bstr_free_buf(&bs); // free not strictly necessary here because of short-string, but always a good idea
+```
--- src/tests.c
+++ src/tests.c
@@ -18,51 +18,63 @@ int
 main()
 {
     bstr *bs;
+    char * cool_text_small = "Some really cool text!";
+    char * cool_text_big = "Some really cool text!Some really cool text!";
+
     /*
      * Short string
      */
-    char * cool_text = "Some really cool text!";
-
     // Create and append from cstring
     bs = bstr_new();
-    bstr_append_from_cstring(bs, cool_text, strlen(cool_text));
-    BASIC_ASSERT(bs, cool_text);
+    bstr_append_from_cstring(bs, cool_text_small, strlen(cool_text_small));
+    BASIC_ASSERT(bs, cool_text_small);
     bstr_free(bs);
 
     // Create from cstring
-    bs = bstr_new_from_cstring(cool_text, strlen(cool_text));
-    BASIC_ASSERT(bs, cool_text);
+    bs = bstr_new_from_cstring(cool_text_small, strlen(cool_text_small));
+    BASIC_ASSERT(bs, cool_text_small);
     bstr_free(bs);
 
     // Create and append from printf
     bs = bstr_new();
-    bstr_append_from_printf(bs, "%s", cool_text);
-    BASIC_ASSERT(bs, cool_text);
+    bstr_append_from_printf(bs, "%s", cool_text_small);
+    BASIC_ASSERT(bs, cool_text_small);
     bstr_free(bs);
 
     /*
      * Long string
      */
-    cool_text = "Some really cool text!Some really cool text!";
-
     // Create and append from cstring
     bs = bstr_new();
-    bstr_append_from_cstring(bs, cool_text, strlen(cool_text));
-    BASIC_ASSERT(bs, cool_text);
+    bstr_append_from_cstring(bs, cool_text_big, strlen(cool_text_big));
+    BASIC_ASSERT(bs, cool_text_big);
     bstr_free(bs);
 
     // Create from cstring
-    bs = bstr_new_from_cstring(cool_text, strlen(cool_text));
-    BASIC_ASSERT(bs, cool_text);
+    bs = bstr_new_from_cstring(cool_text_big, strlen(cool_text_big));
+    BASIC_ASSERT(bs, cool_text_big);
     bstr_free(bs);
 
     // Create and append from printf
     bs = bstr_new();
-    bstr_append_from_printf(bs, "%s", cool_text);
-    BASIC_ASSERT(bs, cool_text);
+    bstr_append_from_printf(bs, "%s", cool_text_big);
+    BASIC_ASSERT(bs, cool_text_big);
     bstr_free(bs);
 
+    /*
+     * Stack Allocate
+     */
 
+    bstr bstack;
+    bstr_init(&bstack);
+    bstr_append_from_printf(&bstack, "%s", cool_text_small);
+    BASIC_ASSERT(&bstack, cool_text_small);
+    bstr_free_buf(&bstack);
+
+    bstr_init(&bstack);
+    bstr_append_from_printf(&bstack, "%s", cool_text_big);
+    BASIC_ASSERT(&bstack, cool_text_big);
+    bstr_free_buf(&bstack);
 
     return 0;
 }
